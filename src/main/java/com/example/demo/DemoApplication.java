a266cb0549df2d2065d977e5ece9f7ad3224f46b
cylee9409 <cylee9409@gmail.com>
2022-12-22 17:00:41 +0900

    [fix] 메서드 순서 변경

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+
 		System.out.println("DemoApplication Start");
 		ParameterStoreTest test = new ParameterStoreTest();
 		test.printProperties();
-		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
