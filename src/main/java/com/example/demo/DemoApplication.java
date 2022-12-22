31da6d40174878dd8145f92b71fa8ba93e8c7bc5
cylee9409 <cylee9409@gmail.com>
2022-12-22 16:38:34 +0900

    add parameter store value

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Value("${/io-back/km0/username}")
+	private static String value;
+
 	public static void main(String[] args) {
 		System.out.println("DemoApplication Start");
+
+		System.out.println(value);
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
