e565752f9d9748fa1680708452d68ab4d16f6fa2
cylee9409 <cylee9409@gmail.com>
2022-12-22 16:43:37 +0900

    print parameter store

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.coveo.configuration.parameterstore.ParameterStorePropertySource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	@Value("${/io-back/km0/username}")
+	@Value("${spring.mysql.km0.username}")
 	private static String value;
+
+	@Value("${test.key}")
+	private static String key;
 
 	public static void main(String[] args) {
 		System.out.println("DemoApplication Start");
-
+		
 		System.out.println(value);
+		System.out.println(key);
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
